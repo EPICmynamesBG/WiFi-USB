e2c903e31c6482ff3b16e500a3e11567724fdb44
EPICmynamesBG <mynamesbg@gmail.com>
2016-06-26 02:02:57 -0400

    Update Main VC, now with container view, toast messages, and better display/network response handling

--- iOS/WiFi-USB/ViewController.swift
+++ iOS/WiFi-USB/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Brandon Groff. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController, WiFiUSBNetworkerDelegate {
@@ -16,16 +17,22 @@ class ViewController: UIViewController, WiFiUSBNetworkerDelegate {
     private let fadeTime: NSTimeInterval = 3.0
     /// Current index in array of background colors
     private var currentColorIndex: Int = 0
+    /// Retain last response from WiFi-USB request
+    private var lastResponse: JsonResponse?
     
     private var networker: WiFiUSBNetworker?
-    @IBOutlet weak var togglePowerButton: PowerButton!
-    @IBOutlet weak var rebootButton: PowerButton!
+    @IBOutlet weak var togglePowerButton: GlowingButton!
+    @IBOutlet weak var rebootButton: GlowingButton!
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var infoViewContainer: UIView!
+    @IBOutlet weak var notificationLabel: FadeLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.networker = WiFiUSBNetworker()
         self.networker?.delegate = self
+        self.disableButtons()
         
     }
     
@@ -37,8 +44,7 @@ class ViewController: UIViewController, WiFiUSBNetworkerDelegate {
                                                                       userInfo: nil,
                                                                       repeats: true)
         self.networker?.getStatus()
-        self.togglePowerButton.animateGlow()
-        self.rebootButton.animateGlow()
+
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -53,6 +59,39 @@ class ViewController: UIViewController, WiFiUSBNetworkerDelegate {
     }
     
     
+    @IBAction func togglePowerButtonTap(sender: GlowingButton) {
+        self.sendToggleSignal()
+    }
+    
+    @IBAction func rebootButtonTap(sender: GlowingButton) {
+        self.sendRebootSignal()
+    }
+    
+    @IBAction func infoButtonTap(sender: AnyObject) {
+        self.showContainerView()
+    }
+    
+    func showContainerView () {
+        self.infoViewContainer.alpha = 0.0
+        self.infoViewContainer.hidden = false
+        UIView.animateWithDuration(0.5, animations: {
+            self.infoViewContainer.alpha = 1.0
+            self.infoButton.alpha = 0.0
+        }) { (complete: Bool) in
+            self.infoButton.hidden = true
+        }
+    }
+    
+    func hideContainerView () {
+        self.infoViewContainer.alpha = 1.0
+        self.infoButton.hidden = false
+        UIView.animateWithDuration(0.5, animations: { 
+            self.infoViewContainer.alpha = 0.0
+            self.infoButton.alpha = 1.0
+        }) { (complete: Bool) in
+            self.infoViewContainer.hidden = true
+        }
+    }
 
     /**
      Animate the change of the view's background color
@@ -69,23 +108,79 @@ class ViewController: UIViewController, WiFiUSBNetworkerDelegate {
     
     private func sendRebootSignal() {
         self.networker?.reboot()
+        self.disableButtons()
     }
     
     private func sendToggleSignal() {
+        if (self.togglePowerButton.enabled == false) {
+            print("Button disabled, but tapped")
+        }
         self.networker?.togglePower()
+        self.disableButtons()
+    }
+    
+    private func disableButtons() {
+        self.togglePowerButton.enabled = false
+        self.togglePowerButton.tintColor = UIColor.darkGrayColor()
+        self.rebootButton.enabled = false
+        self.rebootButton.tintColor = UIColor.darkGrayColor()
+    }
+    
+    private func enableButtons() {
+        if ( lastResponse != nil) {
+            if lastResponse?.on != nil {
+                if (lastResponse!.on!){
+                    self.togglePowerButton.tintColor = ColorPalette.SecondaryA.Lightest
+                    self.togglePowerButton.imageGlow = ColorPalette.Black
+                } else {
+                    self.togglePowerButton.tintColor = ColorPalette.Red
+                    self.togglePowerButton.imageGlow = ColorPalette.White
+                }
+            }
+        }
+        self.togglePowerButton.enabled = true
+        self.rebootButton.enabled = true
+        self.rebootButton.tintColor = ColorPalette.Primary.Normal
+    }
+    
+    func showNotification(message: String) {
+        self.notificationLabel.text = message
+        self.notificationLabel.showForDuration(3.0)
     }
     
     /* ----- WiFiUSBNetworkerDelegate Functions ----- */
     
     func WiFiUSBStatus(response: JsonResponse) {
-        //TODO
+        self.lastResponse = response
+        if (self.networker!.rebooting) {
+            self.lastResponse = nil
+            self.notificationLabel.text = "Rebooting. Device is OFFLINE"
+            self.notificationLabel.hidden = false
+        } else {
+            self.showNotification(response.description!)
+            self.enableButtons()
+        }
+        
     }
     
     func WiFiUSBRequestError(error: NSError?, message: String?) {
-        let popup = UIAlertController(title: "Network Request Error", message: message, preferredStyle: .Alert)
-        popup.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-        self.presentViewController(popup, animated: true, completion: nil)
+        if (!self.networker!.rebooting) {
+            self.showNotification(message!)
+        }
+    }
+    
+    func WiFiUSBRebootComplete(statusResponse: JsonResponse) {
+        self.lastResponse = statusResponse
+        self.notificationLabel.text = "Reboot complete. Device is ONLINE"
+        self.notificationLabel.showForDuration(3.0)
+        self.enableButtons()
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "infoEmbed") {
+            let infoEmbeddedView = segue.destinationViewController as! InfoViewController
+            infoEmbeddedView.parent = self
+        }
+    }
 }
 
