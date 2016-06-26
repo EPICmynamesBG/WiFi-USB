af166d9a9dd5a6b6bc2d38113d058da4c33df519
EPICmynamesBG <mynamesbg@gmail.com>
2016-06-26 18:01:46 -0400

    Update LISCENSE and header comments

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -1,3 +1,9 @@
+/**
+* WiFi-USB: a wirelessly controllable USB power port
+* Author: Brandon Groff
+* Version: 1.0
+* License: GNU GPLv3 (see LICENSE)
+*/
 #pragma once
 
 #define SSID "YOUR-WIFI-NAME"
